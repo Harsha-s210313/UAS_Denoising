553cec0b57945650f50485d7d63d5cbfa6b32391
Kannajosyula Harsha Vardhan <harshakannajosyula@gmail.com>
2026-08-12 22:15:48 +0530

    Refactor CNN model to SignalDetectorCNN with updates

--- code/detector.py
+++ code/detector.py
@@ -1,54 +1,53 @@
 import torch
 import torch.nn as nn
-from config import Config
 
 
-class Configurable1DCNN(nn.Module):
-    def __init__(self, config=None):
-        super(Configurable1DCNN, self).__init__()
-
-        if config is None:
-            config = Config.DETECTOR_CONFIG
-
+class SignalDetectorCNN(nn.Module):
+    """
+    Stage 1 Binary Classification Model.
+    Detects whether an acoustic pulse/echo signature is present in the signal window.
+    """
+    def __init__(self, config):
+        super(SignalDetectorCNN, self).__init__()
         in_channels = config.get("input_channels", 1)
-        conv_filters = config.get("conv_filters", [8, 16, 32, 64])
+        filters = config.get("conv_filters", [16, 32, 64, 128])
         kernel_sizes = config.get("kernel_sizes", [7, 5, 5, 3])
-        fc_units = config.get("fc_units", 32)
-        dropout_rate = config.get("dropout", 0.5)
+        fc_units = config.get("fc_units", 64)
+        dropout = config.get("dropout", 0.1)
 
         layers = []
-        current_channels = in_channels
+        curr_channels = in_channels
 
-        for out_channels, kernel_size in zip(conv_filters, kernel_sizes):
+        for num_filters, k_size in zip(filters, kernel_sizes):
             layers.append(
                 nn.Conv1d(
-                    in_channels=current_channels,
-                    out_channels=out_channels,
-                    kernel_size=kernel_size,
-                    padding=kernel_size // 2,
+                    curr_channels,
+                    num_filters,
+                    kernel_size=k_size,
+                    padding=k_size // 2,
                 )
             )
-            layers.append(nn.BatchNorm1d(out_channels))
-            layers.append(nn.ReLU())
-            layers.append(nn.MaxPool1d(kernel_size=2))
-            current_channels = out_channels
+            layers.append(nn.BatchNorm1d(num_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            layers.append(nn.MaxPool1d(kernel_size=2, stride=2))
+            if dropout > 0.0:
+                layers.append(nn.Dropout(dropout))
+            curr_channels = num_filters
 
-        self.features = nn.Sequential(*layers)
-
-        # Adaptive pool fixes output length to 64 regardless of dynamic input length
-        self.adaptive_pool = nn.AdaptiveAvgPool1d(64)
-        flattened_dim = conv_filters[-1] * 64
+        self.conv_blocks = nn.Sequential(*layers)
+        self.global_pool = nn.AdaptiveAvgPool1d(1)
 
         self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(flattened_dim, fc_units),
-            nn.ReLU(),
-            nn.Dropout(dropout_rate),
-            nn.Linear(fc_units, 1)
+            nn.Linear(filters[-1], fc_units),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(fc_units, 1),
+            nn.Sigmoid(),
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = self.adaptive_pool(x)
-        x = self.classifier(x)
-        return x
+        x = self.conv_blocks(x)
+        x = self.global_pool(x)
+        x = x.squeeze(-1)
+        out = self.classifier(x)
+        return out.squeeze(-1)
